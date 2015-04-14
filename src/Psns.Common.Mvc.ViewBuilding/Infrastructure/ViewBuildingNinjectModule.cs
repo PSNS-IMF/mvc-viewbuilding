9ea0b269edec4239b41e26a6ceca2ab0de5b3828
Jeff Purcell <jeffrey.a.purcell@navy.mil>
2015-04-14 11:35:57 -0700

    Adding a Ninject module that will bind ICrudViewBuilder to the CrudViewBuilder.

---
+++ src/Psns.Common.Mvc.ViewBuilding/Infrastructure/ViewBuildingNinjectModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Ninject.Modules;
+
+using Psns.Common.Mvc.ViewBuilding.ViewBuilders;
+
+namespace Psns.Common.Mvc.ViewBuilding.Infrastructure
+{
+    /// <summary>
+    /// Contains a binding for the CrudViewBuilder
+    /// </summary>
+    public class ViewBuildingNinjectModule : NinjectModule
+    {
+        /// <summary>
+        /// Binds ICrudViewBuilder to CrudViewBuilder
+        /// </summary>
+        public override void Load()
+        {
+            Bind<ICrudViewBuilder>().To<CrudViewBuilder>();
+        }
+    }
+}
